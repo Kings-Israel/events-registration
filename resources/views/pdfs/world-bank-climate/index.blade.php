8713aa5c7afb9c700b7abbc0edacbaa3a5ea57a2
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-11-16 20:09:04 +0300

    Update

--- resources/views/pdfs/world-bank-climate/index.blade.php
+++ resources/views/pdfs/world-bank-climate/index.blade.php
@@ -5,20 +5,25 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <br/>
     <style id="fonts1" type="text/css" >
-        @font-face {
-            font-family: 'FranklinGothic-Book_16';
-            src: url("fonts/FranklinGothic-Book_16.woff") format("woff");
-        }
+        	@font-face {
+                font-family: FranklinGothic-Book_16;
+                src: url("FranklinGothic-Book_16.ttf") format("truetype");
+            }
 
-        @font-face {
-            font-family: 'FranklinGothic-MediumItalic_1e';
-            src: url("fonts/FranklinGothic-MediumItalic_1e.woff") format("woff");
-        }
+            @font-face {
+                font-family: FranklinGothic-MediumItalic_1e;
+                src: url("http://127.0.0.1:8000/fonts/FranklinGothic-MediumItalic_1e.ttf") format("truetype");
+            }
 
-        @font-face {
-            font-family: 'Korolev-Medium_1c';
-            src: url("fonts/Korolev-Medium_1c.woff") format("woff");
-        }
+            @font-face {
+                font-family: 'korolev medium';
+                src: url("Korolev-Medium_1c.ttf") format("truetype");
+            }
+
+            @font-face {
+                font-family: 'FranklinGothic-MediumItalic_1e';
+                src: url({{ storage_path('fonts/FranklinGothic-MediumItalic_1e.ttf') }}) format("truetype");
+            }
     </style>
     <style type="text/css">
         *,*::before,*::after {
@@ -26,12 +31,12 @@
             padding: 0;
         }
         p {margin: 0; padding: 0;}
-        .ft10{font-size:34px;font-family:Times;color:#63c13d;}
-        .ft11{font-size:78px;font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;color:#ffffff;}
-        .ft12{font-size:60px;font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;color:#ffffff; white-space: pre-wrap}
-        .ft13{font-size:40px;font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;color:#ffffff; white-space: pre-wrap}
-        .ft14{font-size:25px;font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;color:#63c13d; white-space: pre-wrap}
-        .ft18{font-size:34px;font-family:Arial, Helvetica, sans-serif;color:#231f20;}
+        .ft10{font-size:34px; font-family:Times;color:#63c13d;}
+        .ft11{font-size:78px; font-family:'Franklin Gothic', sans-serif; color:#ffffff;}
+        .ft12{font-size:60px; font-weight: 400; font-family:'Franklin Gothic Medium', sans-serif; color:#ffffff; white-space: pre-wrap}
+        .ft13{font-size:40px; font-weight: bold; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; color:#ffffff; white-space: pre-wrap; max-width: 460px;}
+        .ft14{font-size:25px; font-weight: bold; font-family:'korolev medium', sans-serif; color:#63c13d; white-space: pre-wrap;}
+        .ft18{font-size:34px; font-family: Arial, Helvetica, sans-serif; color:#231f20;}
     </style>
 </head>
 <body bgcolor="#A0A0A0" vlink="blue" link="blue">
@@ -39,15 +44,15 @@
         <img width="793" height="1103" src="{{ public_path('assets/world-bank-climate/img/target001.png') }}" alt="background image"/>
         <p style="position:absolute;top:126px;left:276px;white-space:nowrap" class="ft11">KENYA</p>
         <p style="position:absolute;top:790px;left:200px;" class="ft12">{{ $visitor->name }}</p>
-        <p style="position:absolute;top:870px;left:200px;" class="ft13"><i>{{ $visitor->Company }}</i></p>
-        <p style="position:absolute;top:970px;left:200px;" class="ft14">{{ $visitor->Role }}</p>
+        <p style="position:absolute;top:870px;left:200px;" class="ft14">{{ Str::upper($visitor->Role) }}</p>
+        <p style="position:absolute;top:950px;left:200px;" class="ft13"><i>{{ $visitor->Company }}</i></p>
         <p style="position:absolute;top:510px;left:268px;white-space:nowrap" class="ft18">
             <img class="qrcodeImg" src="data:image/png;base64,{!! DNS2D::getBarcodePNG((string) $visitor->id, 'QRCODE', 12.4, 12.4) !!}" />
         </p>
     </div>
 </body>
 </html>
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="" xml:lang="">
 <head>
 <title></title>
@@ -71,4 +76,4 @@
         <p style="position:absolute;top:126px;left:273px;white-space:nowrap" class="ft21">KENYA</p>
     </div>
 </body>
-</html>
+</html> --}}
