4e9a8fe0347452d44f251ee876e2c2fe5ce153e9
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-11-20 12:21:39 +0300

    UPdate

--- resources/views/view-session.blade.php
+++ resources/views/view-session.blade.php
@@ -55,45 +55,7 @@
                         <h6>{{ $session->visitors->count() }}</h6>
                     </span>
                 </div>
-                <div class="card-body">
-                    <table class="border-collapse table-auto w-full text-sm bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25">
-                        <thead>
-                        <tr>
-                            <th class="py-1 px-1 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-left">
-                                {{ __('User Names') }}
-                            </th>
-
-                            <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-left">
-                                {{ __('Country') }}
-                            </th>
-                            <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-left">
-                                {{ __('Role') }}
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody class="bg-gray-200 dark:bg-slate-800">
-                        @foreach($session->visitors as $guest)
-                            <tr>
-                                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                    <div class="text-sm text-gray-200">
-                                        {{ $guest->salutation ?? '' }} {{ $guest->name }}
-                                    </div>
-                                </td>
-                                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                    <div class="text-sm text-gray-200">
-                                        {{ $guest->Country }}
-                                    </div>
-                                </td>
-                                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                    <div class="text-sm text-gray-200">
-                                        {{ $guest->Role ?? 'NA' }}
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                <livewire:session-guest-list :session="$session" />
             </div>
         @else
             <div class="card mt-1 sm:px-6 lg:px-8 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg max-w-7xl mx-auto">
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\VisitorController;
+use App\Models\Visitor;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/sessions', [SessionController::class, 'index']);
 Route::get('/visitor/{id}', [VisitorController::class, 'show']);
@@ -12,3 +14,18 @@ Route::post('/register/session', [VisitorController::class, 'registerToSession']
 Route::post('/session/store', [SessionController::class, 'store']);
 Route::post('/visitor/store', [VisitorController::class, 'store']);
 Route::post('/visitor/session/store', [VisitorController::class, 'registerToSession']);
+
+Route::get('/sessions/attendance/update', function () {
+    $visitors = Visitor::whereDate('created_at', '2023-11-17')->get();
+
+    foreach ($visitors as $visitor) {
+        $attendance = DB::table('visitor_session')->get()->pluck('visitor_id');
+
+        if (!collect($attendance)->contains($visitor->id)) {
+            DB::table('visitor_session')->create([
+                'session_id' => 1,
+                'visitor_id' => $visitor->id
+            ]);
+        }
+    }
+});
